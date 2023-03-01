1938c4b111a5491780060101fdd63b3afcdc329e
jodiambra <jodiambra@gmail.com>
2023-02-28 23:32:56 -0500

    model implementation testing

--- app_test2.py
+++ app_test2.py
@@ -1,8 +1,11 @@
 from prediction import predict, predict_proba
+import numpy as np 
 
-result = predict([[2, 46, 1, 400, 25]])
-proba = predict_proba([[2, 46, 1, 400, 25]]) * 100
+data = np.array([[2, 46, 1, 400, 25]])
+np.save('data.npy', data, allow_pickle=True)
+data = np.load('data.npy', allow_pickle=True)
+
+result = predict(data)
+proba = predict_proba(data) * 100
 
 print(result, proba)
-
-
